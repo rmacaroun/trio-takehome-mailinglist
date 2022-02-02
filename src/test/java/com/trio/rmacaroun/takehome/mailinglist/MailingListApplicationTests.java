1ae9033e961c87613f15fe12d9f2736418bfa730
Rafael Macaroun <rmacaroun@hotmail.com>
2022-02-02 11:38:12 -0300

    #2: Base Project

---
+++ src/test/java/com/trio/rmacaroun/takehome/mailinglist/MailingListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trio.rmacaroun.takehome.mailinglist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailingListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
