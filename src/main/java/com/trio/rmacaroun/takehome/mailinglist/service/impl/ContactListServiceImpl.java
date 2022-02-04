bb0ee16519ba0e8daad1ada171aba92e7a05c851
Rafael Macaroun <rmacaroun@hotmail.com>
2022-02-04 20:46:50 -0300

    #7: Adjusting the code to use Feign Clients

--- src/main/java/com/trio/rmacaroun/takehome/mailinglist/service/impl/ContactListServiceImpl.java
+++ src/main/java/com/trio/rmacaroun/takehome/mailinglist/service/impl/ContactListServiceImpl.java
@@ -1,15 +1,12 @@
 package com.trio.rmacaroun.takehome.mailinglist.service.impl;
 
+import com.trio.rmacaroun.takehome.mailinglist.client.ContactListClient;
 import com.trio.rmacaroun.takehome.mailinglist.dto.Contact;
 import com.trio.rmacaroun.takehome.mailinglist.service.ContactListService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -17,16 +14,12 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ContactListServiceImpl implements ContactListService {
 
-    @Value("${application.mockapi.contact.address}")
-    private String mockApiContactAddress;
-
-    private final RestTemplate restTemplate;
+    private final ContactListClient contactListClient;
 
     @Override
     public List<Contact> fetchAllContacts() {
-        final ResponseEntity<Contact[]> responseEntity = this.restTemplate.getForEntity(mockApiContactAddress, Contact[].class);
-        final Contact[] contacts = responseEntity.getBody();
-        log.info("Number of Available Contacts: {}", contacts.length);
-        return Arrays.asList(contacts); // TODO manage null response
+        final List<Contact> contacts = this.contactListClient.fetchAllContacts();
+        log.info("Contacts were fetched successfully. Number of Available Contacts: {}", contacts.size());
+        return contacts; // TODO manage exceptions
     }
 }
--- src/main/java/com/trio/rmacaroun/takehome/mailinglist/service/impl/MailchimpServiceImpl.java
+++ src/main/java/com/trio/rmacaroun/takehome/mailinglist/service/impl/MailchimpServiceImpl.java
@@ -1,7 +1,6 @@
 package com.trio.rmacaroun.takehome.mailinglist.service.impl;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.trio.rmacaroun.takehome.mailinglist.client.MailchimpClient;
 import com.trio.rmacaroun.takehome.mailinglist.dto.Contact;
 import com.trio.rmacaroun.takehome.mailinglist.dto.Member;
 import com.trio.rmacaroun.takehome.mailinglist.mapper.ContactMemberMapper;
@@ -10,81 +9,40 @@ import com.trio.rmacaroun.takehome.mailinglist.service.MailchimpService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.*;
-
-import static org.springframework.http.HttpMethod.PUT;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class MailchimpServiceImpl implements MailchimpService {
 
-    @Value("${application.mailchimp.api.username}")
-    private String username;
-
-    @Value("${application.mailchimp.api.baseurl}")
-    private String baseUrl;
-
-    @Value("${application.mailchimp.api.key}")
-    private String apiKey;
-
     @Value("${application.mailchimp.api.audience-id}")
     private String audienceId;
-
-    @Value("${application.mailchimp.api.endpoint.add-member}")
-    private String addMemberEndpoint; // TODO fix hard-coded
-
-    @Value("${application.mailchimp.api.endpoint.update-member}")
-    private String updateMemberEndpoint; // TODO fix hard-coded
-
-    @Value("${application.mailchimp.api.endpoint.search-member}")
-    private String searchMemberEndpoint; // TODO fix hard-coded
-
-    private final RestTemplate restTemplate;
-
-    private final ObjectMapper objectMapper;
 
     private final ContactMemberMapper contactMemberMapper;
 
     private final ContactListService contactListService;
 
+    private final MailchimpClient mailchimpClient;
+
     @Override
     public List<Contact> addOrUpdateAudienceMembers() {
         final List<Member> members = new ArrayList<>();
         final List<Contact> contacts = this.contactListService.fetchAllContacts();
-        contacts.stream().forEach(contact ->
+        contacts.forEach(contact ->
             updateAudienceMember(contact).ifPresent(members::add)
         );
         return this.contactMemberMapper.mapAsList(members, Contact.class);
     }
 
     private Optional<Member> updateAudienceMember(final Contact contact) {
-        final String addMemberUrl = baseUrl + updateMemberEndpoint;
         final Member member = this.contactMemberMapper.map(contact, Member.class);
-        final Map<String, String> query = new HashMap<>();
-        query.put("subscriber_hash", member.getEmailAddress());
-        final ResponseEntity<Member> responseEntity = this.restTemplate.exchange(addMemberUrl, PUT, setupHttpEntity(member), Member.class, query);
-        return Optional.ofNullable(responseEntity.getBody());
-    }
-
-    private HttpEntity<String> setupHttpEntity(Member member) {
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.setBasicAuth(username, apiKey);
-        String body = null;
-        try {
-            if (member != null) {
-                body = this.objectMapper.writeValueAsString(member);
-            }
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
-        final HttpEntity<String> httpEntity = new HttpEntity<>(body, httpHeaders);
-        return httpEntity;
+        final Member updatedMember = this.mailchimpClient.updateAudienceMember(audienceId, member.getEmailAddress(), member);
+        log.debug("Member {} updated successfully", updatedMember.getEmailAddress());
+        return Optional.of(updatedMember); // TODO manage exceptions
     }
 }
